bbc3e7063956deeb301d6f35a6976676d67767f2
oona-k <59806531+oona-k@users.noreply.github.com>
2021-08-19 20:49:22 +0300

    backend commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+var cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.listen(3001, () => console.log("Server listening at port 3001"));
+
+const fileUrl =
+  "https://media.wizards.com/2021/downloads/MagicCompRules%2020210419.txt";
+
+
+app.get("/getrules", (req, res) => {
+  const https = require("https");
+  let myURL = new URL(fileUrl);
+  let body = [];
+  https
+    .request(myURL, (response) => {
+      response.on("data", (chunk) => body.push(chunk));
+      response.on("end", () => res.send(body.toString()));
+    })
+    .end();
+});
